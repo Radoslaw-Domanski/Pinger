8746429a32fcf200f862ae3a006ea322bbab216b
Radosław Domański <rdomanski94@gmail.com>
2017-06-22 18:38:08 +0200

    Pinger

---
+++ Pinger/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Pinger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.plikInfo = new System.Windows.Forms.Label();
+            this.wczytanoInfo = new System.Windows.Forms.Label();
+            this.spingowanoInfo = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.zapisaneInfo = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ping";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Domena :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Adres IP:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Wczytaj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(240, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Zapisz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Załadowany plik :";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(120, 170);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Pinguj Domeny";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // plikInfo
+            // 
+            this.plikInfo.AutoSize = true;
+            this.plikInfo.Location = new System.Drawing.Point(146, 141);
+            this.plikInfo.Name = "plikInfo";
+            this.plikInfo.Size = new System.Drawing.Size(29, 13);
+            this.plikInfo.TabIndex = 9;
+            this.plikInfo.Text = "Brak";
+            // 
+            // wczytanoInfo
+            // 
+            this.wczytanoInfo.AutoSize = true;
+            this.wczytanoInfo.Location = new System.Drawing.Point(36, 196);
+            this.wczytanoInfo.Name = "wczytanoInfo";
+            this.wczytanoInfo.Size = new System.Drawing.Size(0, 13);
+            this.wczytanoInfo.TabIndex = 10;
+            // 
+            // spingowanoInfo
+            // 
+            this.spingowanoInfo.AutoSize = true;
+            this.spingowanoInfo.Location = new System.Drawing.Point(117, 196);
+            this.spingowanoInfo.Name = "spingowanoInfo";
+            this.spingowanoInfo.Size = new System.Drawing.Size(0, 13);
+            this.spingowanoInfo.TabIndex = 11;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(39, 280);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(276, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 13;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(117, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Progres Pingowania";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(184, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Poprawny format domeny : domena.pl";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Załadowano domen :";
+            // 
+            // zapisaneInfo
+            // 
+            this.zapisaneInfo.AutoSize = true;
+            this.zapisaneInfo.Location = new System.Drawing.Point(36, 242);
+            this.zapisaneInfo.Name = "zapisaneInfo";
+            this.zapisaneInfo.Size = new System.Drawing.Size(0, 13);
+            this.zapisaneInfo.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(444, 49);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(441, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Adres IP:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(329, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(106, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Historia Pingowania :";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(332, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(103, 95);
+            this.listBox1.TabIndex = 22;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(332, 151);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 23);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Wyczyść listę";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 313);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.zapisaneInfo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.spingowanoInfo);
+            this.Controls.Add(this.wczytanoInfo);
+            this.Controls.Add(this.plikInfo);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pinger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label plikInfo;
+        private System.Windows.Forms.Label wczytanoInfo;
+        private System.Windows.Forms.Label spingowanoInfo;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label zapisaneInfo;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Pinger/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Runtime.InteropServices;
+using Microsoft.Office.Interop.Excel;
+
+namespace Pinger
+{
+    public partial class Form1 : Form
+    {
+        public static ArrayList listaDomen = new ArrayList();
+        public static ArrayList listaIP = new ArrayList();
+        public static ArrayList historiaDomen = new ArrayList();
+        public static ArrayList historiaIP = new ArrayList();
+        public static int rozmiar = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                textBox1.Text = String.Empty;
+                textBox2.Text = String.Empty;
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {            
+            if (e.KeyCode == Keys.Escape)
+            {
+                textBox2.Text = String.Empty;
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                textBox3.Text = String.Empty;
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string domena = textBox1.Text;
+            domena = domena.Trim();
+            if (domena.StartsWith("http://"))
+            {
+                domena = domena.Substring("http://".Length);
+            }
+            string ip = pingIp(domena);
+            listBox1.Items.Add(domena);
+            textBox2.Text = ip;
+            historiaDomen.Add(domena);
+            historiaIP.Add(ip);
+        }
+        public static String pingIp(string domena)
+        {
+            String spingowane = String.Empty;
+            domena = domena.Trim();
+            if (domena.StartsWith("http://"))
+            {
+                domena = domena.Substring("http://".Length);
+            }
+            try
+            {
+                IPAddress[] ip = Dns.GetHostAddresses(domena);
+                spingowane = ip[0].ToString();
+            }
+            catch (Exception e)
+            {
+                spingowane = "Domena nieosiągalna !";
+            }
+            
+            return spingowane;
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listaDomen.Clear();
+            listaIP.Clear();
+            rozmiar = 0;
+            progressBar1.Value = 0;
+            openFileDialog1.Filter = "Notepad|*.txt|All|*.*";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {                
+                    System.IO.StreamReader strumien = new System.IO.StreamReader(openFileDialog1.FileName);
+                    plikInfo.Text = Path.GetFileName(openFileDialog1.FileName);
+                    while (!strumien.EndOfStream)
+                        listaDomen.Add(strumien.ReadLine());
+                    strumien.Close();
+                    label5.Text = listaDomen.Count.ToString();
+                    progressBar1.Maximum = listaDomen.Count;                
+            }            
+            wczytanoInfo.Text = "Plik załadowano !";
+            spingowanoInfo.Text = String.Empty;
+            zapisaneInfo.Text = String.Empty;
+        }
+        public static void pingList(ArrayList lista){
+            foreach(String domena in lista){
+                listaIP.Add(pingIp(domena));
+                rozmiar++;
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String plik = String.Empty; 
+            saveFileDialog1.Filter = "All|*.*|Notepad|*.txt";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                plik = saveFileDialog1.FileName;
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.WriteLine("Domena" + "\t" + "Adres IP");
+                    for (int i = 0; i < rozmiar; i++)
+                        sw.WriteLine(listaDomen[i] + "\t" + listaIP[i]);
+                    sw.Close();
+                }                                                                                
+            }
+            plikInfo.Text = "Brak";
+            wczytanoInfo.Text = String.Empty;
+            spingowanoInfo.Text = String.Empty;
+            zapisaneInfo.Text = "Zapisano do pliku: " + Path.GetFileName(plik);
+            progressBar1.Value = 0;
+            label5.Text = String.Empty;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (String domena in listaDomen)
+            {
+                listaIP.Add(pingIp(domena));
+                rozmiar++;
+                progressBar1.Value++;
+            }
+            wczytanoInfo.Text = String.Empty;
+            spingowanoInfo.Text = "Spingowano domeny !";
+            zapisaneInfo.Text = String.Empty;
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int x = listBox1.SelectedIndex;
+            textBox3.Text = historiaIP[x].ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            historiaIP.Clear();
+            historiaDomen.Clear();
+            textBox3.Text = String.Empty;
+        }        
+    }
+}
